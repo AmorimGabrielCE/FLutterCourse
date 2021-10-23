5a6e9e048dee9835eb4da1f10c2ad211c82a18ec
Gabriel Amorim <amorimdevelopment@gmail.com>
2021-10-23 02:01:02 -0300

    Initial commit with some work about adding and learning Layout Widgets

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favorite color?',
+      'What\s your favorite animal?',
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        backgroundColor: Color(0000000),
+      ),
+      body: Column(
+        children: [
+          Text("The question!"),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer '),
+            onPressed: null,
+          ),
+        ],
+      ),
+    ));
+  }
+}
